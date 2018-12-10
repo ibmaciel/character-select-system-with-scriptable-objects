9a60d3d20e3a046e347e218b1c0662ba00e08b1c
ibmaciel <ibmaciel@users.noreply.github.com>
2018-12-10 17:31:41 -0200

    Complete

---
+++ Character Select System with Scriptable Objects/Assets/Scripts/Character.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "Character")]
+public class Character : ScriptableObject
+{
+
+    public string characterName = "Default";
+    public int startingHp = 100;
+
+    public Ability[] characterAbilities;
+
+}
